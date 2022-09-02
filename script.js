ee9a3737113700ebea50234c01fc4f139c6d739d
Collins Muiruri Kamau <collins.muiruri@student.moringaschool.com>
2022-09-02 17:59:47 +0300

    update

--- script.js
+++ script.js
@@ -16,3 +16,45 @@ const getMovie = async () => {
               `;
     });
   };
+  const searchMovie = () => {
+    document.getElementById("getmovie").addEventListener("click", async (e) => {
+      e.preventDefault();
+      const inputValue = document.querySelector("#search-movies").value;
+      const searchMovie = await fetch(
+        `https://api.tvmaze.com/search/shows?q=${inputValue}`
+      );
+      const searchedMovies = await searchMovie.json();
+      console.log(searchedMovies);
+      document.querySelector(".display-movies").innerHTML = "";
+      searchedMovies.forEach((searchMovie) => {
+        if (searchMovie.show.image !== null) {
+          document.querySelector(".display-movies").innerHTML += `
+                      <div class="each-displayed-movie">
+                      <h2>${searchMovie.show.name}</h2>
+                    <img src=${searchMovie.show.image.medium}>
+                    <p>${searchMovie.show.genres}</p>
+                
+          
+                  </div>
+                      
+                      
+                      `;
+        }
+      });
+  
+      document.querySelector(".show-all-movies").innerHTML = `
+            <button class="show-all-btn">SHOW ALL MOVIES</button>
+            
+            `;
+      document
+        .querySelector(".show-all-movies")
+        .addEventListener("click", (e) => {
+          e.preventDefault();
+          document.querySelector(".display-movies").innerHTML = "";
+          document.querySelector("#search-movies").value = "search a movie..";
+          getMovie();
+        });
+    });
+  };
+  searchMovie();
+  getMovie();
